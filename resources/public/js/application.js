2eb695635b00f7cf1c2d9a352aa73833a23737cc
dillius <dillius@gmail.com>
2013-09-26 11:17:22 -0400

    Corrected unit display for calculation results when using imperial units. Corrected title entries for project/page names.

--- resources/public/js/application.js
+++ resources/public/js/application.js
@@ -6,6 +6,8 @@ function cleanUnitDisplay(unitParts) {
     var degree = unitParts[0], unit = unitParts[1];
     if(degree == "base") {
         return unit + "(s)";
+    } else if(unit === undefined) {
+        return degree + "(s)";
     } else {
         return degree + unit + "(s)";
     }
